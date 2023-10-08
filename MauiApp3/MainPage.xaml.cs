801b54fb8aa10bcd613d4aa4e36a03ac80e9fb52
Comlab <Comlab@CMLB2-29>
2023-10-09 07:52:59 +0800

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    async void OnDial(object sender, EventArgs e)
+    {
+		Button button = (Button)sender;
+		try
+		{
+			PhoneDialer.Open(PhoneNum.Text);
+		}
+		catch
+		{
+			await DisplayAlert("Invalid Input", "Please enter a valid phone number", "OK");
+		}
+    }
+
+    
+
+	
+}
+
